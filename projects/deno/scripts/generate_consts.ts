520c4b369759842af80092d0264953b67f10b8b5
Juliette Cordor <me@cordor.dev>
2025-03-13 14:59:42 +1100

    updated consts genration

--- projects/deno/scripts/generate_consts.ts
+++ projects/deno/scripts/generate_consts.ts
@@ -8,23 +8,21 @@ export function generateConsts() {
 
   const formatCode = (code: (typeof codeRanges)[0]["codes"][0]): string =>
     `/**
-    * HTTP Code for ${code.message} (${code.code})
-    * @see {@link https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/${
+* HTTP Code for ${code.message} (${code.code})
+* @see {@link https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/${
       code.code
     } ${code.message}}
-    */
-    export const ${constantCase(code.message)} = ${code.code};
-    `;
+*/
+export const ${constantCase(code.message)} = ${code.code};\n`;
 
   const formatRange = (range: (typeof codeRanges)[0]): string =>
     `/**
-    * HTTP Codes for ${range.name} responses (${range.range})
-    * @see {@link https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#information_responses}
-    */
-    export const ${constantCase(range.name)} = {
-        ${range.codes.map((code) => constantCase(code.message)).join(",\n    ")}
-    };
-    `;
+* HTTP Codes for ${range.name} responses (${range.range})
+* @see {@link https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#information_responses}
+*/
+export const ${constantCase(range.name)} = {
+${range.codes.map((code) => constantCase(code.message)).join(",\n")}
+};\n`;
 
   codeRanges.map((range) => {
     range.codes.map((code) => {
